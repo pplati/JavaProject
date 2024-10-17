bd31dffcab2f4308ab927b63243455430387e3af
dPetrotos <dpetrotos@outlook.com>
2024-10-17 19:54:24 +0300

    Added TravelCompanyEShop.

---
+++ src/com/travelcompany/eshop/TravelCompanyEShop.java
@@ -0,0 +1,13 @@
+package com.travelcompany.eshop;
+
+import com.travelcompany.eshop.domain.Customer;
+import com.travelcompany.eshop.enumeration.Category;
+import com.travelcompany.eshop.enumeration.Nationality;
+
+public class TravelCompanyEShop {
+    public static void main(String[] args) {
+        Nationality nationality = Nationality.valueOf("italian".toUpperCase());
+        System.out.println(nationality);
+        Customer businessCustomer = new Customer(1L, "Dionysis", "d.petrotos@codehub.gr", "Stadiou 2", Nationality.GREEK, Category.BUSINESS);
+    }
+}
